dc6d6529ea295eb90409733e505b91162a31951d
Paulina Żurawska <zurawskapa@gmail.com>
2019-01-10 08:58:43 +0100

    zad06

--- zad06/src/main/java/com/example/pzurawska/zad06/rest/HorseRESTService.java
+++ zad06/src/main/java/com/example/pzurawska/zad06/rest/HorseRESTService.java
@@ -1,9 +1,8 @@
 package com.example.pzurawska.zad06.rest;
 
-import java.util.List;
 
-import javax.ejb.Stateless;
-import javax.inject.Inject;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -17,51 +16,44 @@ import javax.ws.rs.core.Response;
 import com.example.pzurawska.zad06.domain.Horse;
 import com.example.pzurawska.zad06.service.HorseManager;
 
-
 @Path("horse")
 public class HorseRESTService {
 	
+	@EJB
+	HorseManager hm;
 	
-	
-	
-	
-	
-	
-	/*
-	@Inject
-	private HorseManager hm;
-
-	@GET
-	@Path("/{name}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Horse getHorse(@PathParam("name") String name) {
-		return hm.getHorse(name);
-	}
-
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Horse> getHorses() {
-		return hm.getAllHorses();
-	}
-
 	@POST
+	@Path("/")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response addHorse(Horse horse) {
+	public Response addHorse(Horse horse){
 		hm.addHorse(horse);
-		return Response.status(201).entity("Horse").build();
+		return Response.status(201).entity("Horse").build(); 
 	}
-
+	
 	@GET
-	@Path("/test")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String test() {
-		return "Dziala!!";
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Horse getHorse(@PathParam("id") Long id){
+		
+		Horse retrieved = hm.getHorse(id);
+		return retrieved;
+	}
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	@SuppressWarnings("unchecked")
+	public List<Horse> getAllHorses(){
+		return hm.getAll();
 	}
 
+	
 	@DELETE
-	public Response clearPersons() {
-		hm.deleteAllHorses();
+	public Response clearHorses(){
+		hm.deletAll();
 		return Response.status(200).build();
-	}*/
+	}
+	
+	
 
 }
